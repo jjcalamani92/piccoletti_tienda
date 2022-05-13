fe4d11ac823e68b308bfef2c8c768c0bb42c21b4
jjcalamani92 <99366654+jjcalamani92@users.noreply.github.com>
2022-05-12 22:42:28 -0400

    3

--- components/Header.tsx
+++ components/Header.tsx
@@ -137,7 +137,7 @@ const navigation = {
 		{ name: "Stores", href: "#" }
 	]
 };
-function classNames(...classes: any[]) {
+function classNames(...classes: string[]) {
 	return classes.filter(Boolean).join(" ");
 }
 
