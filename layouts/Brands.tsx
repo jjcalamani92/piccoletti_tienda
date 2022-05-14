18566e9675e1bd9e4b4789e603046c70995c3003
jjcalamani92 <99366654+jjcalamani92@users.noreply.github.com>
2022-05-13 23:00:14 -0400

    ss

--- layouts/Brands.tsx
+++ layouts/Brands.tsx
@@ -13,28 +13,28 @@ export const BrandsLayout = () => {
 			</div>
 			<div className="xl:py-16 lg:py-16 md:py-16 sm:py-16 px-15 flex flex-wrap">
 				<div className="w-6/12 xl:w-1/4 lg:w-1/4 md:w-1/4 flex justify-center xl:border-b lg:border-b xl:border-r lg:border-r :border-r border-gray-200 xl:pb-10 pb-16 items-center">
-					<img src="https://cdn.tuk.dev/assets/adidas-dark.png" alt />
+					<img src="https://cdn.tuk.dev/assets/adidas-dark.png" alt="" />
 				</div>
 				<div className="w-6/12 xl:w-1/4 lg:w-1/4 md:w-1/4 flex justify-center xl:border-b lg:border-b xl:border-r lg:border-r border-gray-200 xl:pb-10 pb-16 items-center">
-					<img src="https://cdn.tuk.dev/assets/channel-dark.png" alt />
+					<img src="https://cdn.tuk.dev/assets/channel-dark.png" alt="" />
 				</div>
 				<div className="w-6/12 xl:w-1/4 lg:w-1/4 md:w-1/4 flex justify-center xl:border-b lg:border-b border-gray-200 xl:pb-10 pb-16 pt-4 items-center">
-					<img src="https://cdn.tuk.dev/assets/nike-dark.png" alt />
+					<img src="https://cdn.tuk.dev/assets/nike-dark.png" alt="" />
 				</div>
 				<div className="w-6/12 xl:w-1/4 lg:w-1/4 md:w-1/4 flex justify-center lg:border-b xl:border-b lg:border-l xl:border-l border-gray-200 xl:pb-10 pb-16 items-center">
-					<img src="https://cdn.tuk.dev/assets/toyota-dark.png" alt />
+					<img src="https://cdn.tuk.dev/assets/toyota-dark.png" alt="" />
 				</div>
 				<div className="w-6/12 xl:w-1/4 lg:w-1/4 md:w-1/4 flex justify-center xl:border-r lg:border-r border-gray-200 xl:pt-10 items-center">
-					<img src="https://cdn.tuk.dev/assets/gs1-dark.png" alt />
+					<img src="https://cdn.tuk.dev/assets/gs1-dark.png" alt="" />
 				</div>
 				<div className="w-6/12 xl:w-1/4 lg:w-1/4 md:w-1/4 flex justify-center xl:border-r lg:border-r border-gray-200 xl:pt-10 items-center">
-					<img src="https://cdn.tuk.dev/assets/berry-dark.png" alt />
+					<img src="https://cdn.tuk.dev/assets/berry-dark.png" alt="" />
 				</div>
 				<div className="w-6/12 xl:w-1/4 lg:w-1/4 md:w-1/4 flex justify-center xl:pt-10 lg:pt-10 md:pt-2 pt-16">
-					<img src="https://cdn.tuk.dev/assets/min-dark.png" alt />
+					<img src="https://cdn.tuk.dev/assets/min-dark.png" alt="" />
 				</div>
 				<div className="w-6/12 xl:w-1/4 lg:w-1/4 md:w-1/4 flex justify-center xl:border-l lg:border-l border-gray-200 xl:pt-10 lg:pt-10 md:pt-2 pt-16">
-					<img src="https://cdn.tuk.dev/assets/honda-dark.png" alt />
+					<img src="https://cdn.tuk.dev/assets/honda-dark.png" alt="" />
 				</div>
 			</div>
 		</div>
